d6047dece9e50baf30945b7be7166a0414794171
David Morais <david@frames.news>
2017-08-15 20:51:29 +0100

    Fix get req

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ app.get('/:timestamp', function(request, response) {
   const timestamp = request.params.timestamp;
   response.json(getTimestampJSON(timestamp));
   response.send('Timestamp: ' + timestamp);
+  app.end();
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
@@ -16,7 +17,7 @@ function getNaturalDate(date) {
     return date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear();
 }
 
-function getTimeStampJson(timestamp) {
+function getTimestampJSON(timestamp) {
     const timeResult = {
         unix: 0,
         natural: 0,
@@ -29,8 +30,8 @@ function getTimeStampJson(timestamp) {
         date = new Date(timestamp)
     }
     if (!isNaN(date.getTime())) {
-        result.unix = date.getTime();
-        result.natural = getNaturalDate(date);
+        timeResult.unix = date.getTime();
+        timeResult.natural = getNaturalDate(date);
     }
-    return result;
+    return timeResult;
 }
